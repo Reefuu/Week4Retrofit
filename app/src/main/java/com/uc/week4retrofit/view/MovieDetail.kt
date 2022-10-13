e35f8cd6e82338bc1f80640e20652a75b3374665
Reefuu <rifqiet@gmail.com>
2022-10-13 18:26:17 +0700

    sasawed

--- app/src/main/java/com/uc/week4retrofit/view/MovieDetail.kt
+++ app/src/main/java/com/uc/week4retrofit/view/MovieDetail.kt
@@ -1,10 +1,13 @@
 package com.uc.week4retrofit.view
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.net.toUri
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.uc.week4retrofit.databinding.ActivityMovieDetailBinding
 import com.uc.week4retrofit.helper.Const
 import com.uc.week4retrofit.viewModel.MoviesViewModel
@@ -30,6 +33,10 @@ class MovieDetail : AppCompatActivity() {
         viewModel.movieDetails.observe(this, Observer{ response->
             binding.tvTitleMovieDetail.apply {
                 text = response.title
+            }
+            binding.imgPosterMovieDetail.apply {
+                val img = "https://image.tmdb.org/t/p/original/" + response.poster_path
+                Glide.with(this).load(img).into(binding.imgPosterMovieDetail)
             }
         })
 
